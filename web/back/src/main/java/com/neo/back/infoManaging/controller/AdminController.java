e8dce08e6f9196516c846910b70906c8aed01594
mueg12 <mueg12@ajou.ac.kr>
2024-06-03 04:26:38 +0900

    ..

--- web/back/src/main/java/com/neo/back/infoManaging/controller/AdminController.java
+++ web/back/src/main/java/com/neo/back/infoManaging/controller/AdminController.java
@@ -1,4 +1,4 @@
-package com.neo.back.authorization.controller;
+package com.neo.back.infoManaging.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
